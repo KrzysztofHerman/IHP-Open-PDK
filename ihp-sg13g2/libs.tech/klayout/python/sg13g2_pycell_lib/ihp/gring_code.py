80ea09f78ffa0c5bf2fb0a43e34924c3f44ec1ea
KrzysztofHerman <herman@ihp-microelectronics.com>
2024-06-11 17:02:49 +0200

    guard ring update, bondpad update
    Signed-off-by: KrzysztofHerman <herman@ihp-microelectronics.com>

--- ihp-sg13g2/libs.tech/klayout/python/sg13g2_pycell_lib/ihp/gring_code.py
+++ ihp-sg13g2/libs.tech/klayout/python/sg13g2_pycell_lib/ihp/gring_code.py
@@ -40,13 +40,12 @@ class gring(DloGen):
             suffix = "G3"
 
         CDFVersion = techparams["CDFVersion"]
-        model = techparams["gring_model"]
         defL = techparams["gring_defL"]
         defW = techparams["gring_defW"]
 
         specs("cdf_version", CDFVersion, "CDF Version")
         specs("Display", "Selected", "Display", ChoiceConstraint(["All", "Selected"]))
-        specs("model", model, "Model name")
+        specs("well", "sub", "Well", ChoiceConstraint(["sub", "nwell"]))
 
         specs("w", defW, "Width")
         specs("l", defL, "Length")
@@ -57,10 +56,13 @@ class gring(DloGen):
 
         self.w = Numeric(params["w"]) * 1e6
         self.l = Numeric(params["l"]) * 1e6
+        self.well = params["well"]
         # self.resistance = Numeric(params['R'])
 
     def genLayout(self):
+        nwelllayer = Layer("NWell")
         psdlayer = Layer("pSD")
+        nsdlayer = Layer("nSD")
         activlayer = Layer("Activ")
         textlayer = Layer("TEXT")
         met1layer = Layer("Metal1")
@@ -73,81 +75,157 @@ class gring(DloGen):
         consize = self.techparams["Cnt_a"]
         conspace = self.techparams["Cnt_b"]
         psdActiv = self.techparams["pSD_c1"]
+        nsdActiv = self.techparams["nSDB_e"]
         contbar_min_len = self.techparams["CntB_a1"]
         contbar_min_width = self.techparams["CntB_a"]
         contbar_act_enc = self.techparams["CntB_c"]
         minpSD = self.techparams["pSD_a"]
+        minnSD = self.techparams["nSDB_a"]
 
-        psdRect1 = Rect(psdlayer, Box(0, 0, self.l, self.w))
-        psdRect2 = Rect(psdlayer, Box(minpSD, minpSD, self.l - minpSD, self.w - minpSD))
+        if self.well == "sub":
+            psdRect1 = Rect(psdlayer, Box(0, 0, self.l, self.w))
+            psdRect2 = Rect(
+                psdlayer, Box(minpSD, minpSD, self.l - minpSD, self.w - minpSD)
+            )
 
-        actRect1 = Rect(
-            activlayer, Box(psdActiv, psdActiv, self.l - psdActiv, self.w - psdActiv)
-        )
-        actRect2 = Rect(
-            activlayer,
-            Box(
-                minpSD - psdActiv,
-                minpSD - psdActiv,
-                self.l - minpSD + psdActiv,
-                self.w - minpSD + psdActiv,
-            ),
-        )
+            actRect1 = Rect(
+                activlayer,
+                Box(psdActiv, psdActiv, self.l - psdActiv, self.w - psdActiv),
+            )
+            actRect2 = Rect(
+                activlayer,
+                Box(
+                    minpSD - psdActiv,
+                    minpSD - psdActiv,
+                    self.l - minpSD + psdActiv,
+                    self.w - minpSD + psdActiv,
+                ),
+            )
 
-        lociRect1 = Rect(
-            locintlayer,
-            Box(
-                minpSD / 2 - contbar_min_width / 2,
-                minpSD / 2 - contbar_min_width / 2,
-                self.l - minpSD / 2 + contbar_min_width / 2,
-                self.w - minpSD / 2 + contbar_min_width / 2,
-            ),
-        )
-        lociRect2 = Rect(
-            locintlayer,
-            Box(
-                minpSD / 2 + contbar_min_width / 2,
-                minpSD / 2 + contbar_min_width / 2,
-                self.l - minpSD / 2 - contbar_min_width / 2,
-                self.w - minpSD / 2 - contbar_min_width / 2,
-            ),
-        )
+            lociRect1 = Rect(
+                locintlayer,
+                Box(
+                    minpSD / 2 - contbar_min_width / 2,
+                    minpSD / 2 - contbar_min_width / 2,
+                    self.l - minpSD / 2 + contbar_min_width / 2,
+                    self.w - minpSD / 2 + contbar_min_width / 2,
+                ),
+            )
+            lociRect2 = Rect(
+                locintlayer,
+                Box(
+                    minpSD / 2 + contbar_min_width / 2,
+                    minpSD / 2 + contbar_min_width / 2,
+                    self.l - minpSD / 2 - contbar_min_width / 2,
+                    self.w - minpSD / 2 - contbar_min_width / 2,
+                ),
+            )
 
-        met1Rect1 = Rect(
-            met1layer,
-            Box(
-                minpSD / 2 - contbar_min_width / 2 - endcap,
-                minpSD / 2 - contbar_min_width / 2 - endcap,
-                self.l - minpSD / 2 + contbar_min_width / 2 + endcap,
-                self.w - minpSD / 2 + contbar_min_width / 2 + endcap,
-            ),
-        )
-        met1Rect2 = Rect(
-            met1layer,
-            Box(
-                minpSD / 2 + contbar_min_width / 2 + endcap,
-                minpSD / 2 + contbar_min_width / 2 + endcap,
-                self.l - minpSD / 2 - contbar_min_width / 2 - endcap,
-                self.w - minpSD / 2 - contbar_min_width / 2 - endcap,
-            ),
-        )
+            met1Rect1 = Rect(
+                met1layer,
+                Box(
+                    minpSD / 2 - contbar_min_width / 2 - endcap,
+                    minpSD / 2 - contbar_min_width / 2 - endcap,
+                    self.l - minpSD / 2 + contbar_min_width / 2 + endcap,
+                    self.w - minpSD / 2 + contbar_min_width / 2 + endcap,
+                ),
+            )
+            met1Rect2 = Rect(
+                met1layer,
+                Box(
+                    minpSD / 2 + contbar_min_width / 2 + endcap,
+                    minpSD / 2 + contbar_min_width / 2 + endcap,
+                    self.l - minpSD / 2 - contbar_min_width / 2 - endcap,
+                    self.w - minpSD / 2 - contbar_min_width / 2 - endcap,
+                ),
+            )
 
-        fgXor([psdRect1], [psdRect2], psdlayer)
-        fgXor([actRect1], [actRect2], activlayer)
-        fgXor([lociRect1], [lociRect2], locintlayer)
-        fgXor([met1Rect1], [met1Rect2], met1layer)
+            fgXor([psdRect1], [psdRect2], psdlayer)
+            fgXor([actRect1], [actRect2], activlayer)
+            fgXor([lociRect1], [lociRect2], locintlayer)
+            fgXor([met1Rect1], [met1Rect2], met1layer)
 
-        psdRect1.destroy()
-        psdRect2.destroy()
-        actRect1.destroy()
-        actRect2.destroy()
-        lociRect1.destroy()
-        lociRect2.destroy()
-        met1Rect1.destroy()
-        met1Rect2.destroy()
+            psdRect1.destroy()
+            psdRect2.destroy()
+            actRect1.destroy()
+            actRect2.destroy()
+            lociRect1.destroy()
+            lociRect2.destroy()
+            met1Rect1.destroy()
+            met1Rect2.destroy()
 
-        # fgOr(x1, x3)
+        if self.well == "nwell":
+            nsdRect1 = Rect(nsdlayer, Box(0, 0, self.l, self.w))
+            nsdRect2 = Rect(
+                nsdlayer, Box(minnSD, minnSD, self.l - minnSD, self.w - minnSD)
+            )
 
-        # dbCreateRect(self,  activlayer, Box(psdActiv, psdActiv, self.l - psdActiv, minpSD - psdActiv))
+            actRect1 = Rect(
+                activlayer,
+                Box(nsdActiv, nsdActiv, self.l - nsdActiv, self.w - nsdActiv),
+            )
+            actRect2 = Rect(
+                activlayer,
+                Box(
+                    minnSD - nsdActiv,
+                    minnSD - nsdActiv,
+                    self.l - minnSD + nsdActiv,
+                    self.w - minnSD + nsdActiv,
+                ),
+            )
 
-        # SetSGq(lbl scale height)
+            lociRect1 = Rect(
+                locintlayer,
+                Box(
+                    minnSD / 2 - contbar_min_width / 2,
+                    minnSD / 2 - contbar_min_width / 2,
+                    self.l - minnSD / 2 + contbar_min_width / 2,
+                    self.w - minnSD / 2 + contbar_min_width / 2,
+                ),
+            )
+            lociRect2 = Rect(
+                locintlayer,
+                Box(
+                    minnSD / 2 + contbar_min_width / 2,
+                    minnSD / 2 + contbar_min_width / 2,
+                    self.l - minnSD / 2 - contbar_min_width / 2,
+                    self.w - minnSD / 2 - contbar_min_width / 2,
+                ),
+            )
+
+            met1Rect1 = Rect(
+                met1layer,
+                Box(
+                    minnSD / 2 - contbar_min_width / 2 - endcap,
+                    minnSD / 2 - contbar_min_width / 2 - endcap,
+                    self.l - minnSD / 2 + contbar_min_width / 2 + endcap,
+                    self.w - minnSD / 2 + contbar_min_width / 2 + endcap,
+                ),
+            )
+            met1Rect2 = Rect(
+                met1layer,
+                Box(
+                    minnSD / 2 + contbar_min_width / 2 + endcap,
+                    minnSD / 2 + contbar_min_width / 2 + endcap,
+                    self.l - minnSD / 2 - contbar_min_width / 2 - endcap,
+                    self.w - minnSD / 2 - contbar_min_width / 2 - endcap,
+                ),
+            )
+            nwellRect = Rect(
+                nwelllayer,
+                Box(0, 0, self.l, self.w),
+            )
+
+            fgXor([nsdRect1], [nsdRect2], nsdlayer)
+            fgXor([actRect1], [actRect2], activlayer)
+            fgXor([lociRect1], [lociRect2], locintlayer)
+            fgXor([met1Rect1], [met1Rect2], met1layer)
+
+            nsdRect1.destroy()
+            nsdRect2.destroy()
+            actRect1.destroy()
+            actRect2.destroy()
+            lociRect1.destroy()
+            lociRect2.destroy()
+            met1Rect1.destroy()
+            met1Rect2.destroy()
