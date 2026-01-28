fe4f9e69609ab085497d933283af4d682775526c
KrzysztofHerman <herman@ihp-microelectronics.com>
2026-01-28 21:15:11 +0000

    Corners added for rsil
    Signed-off-by: KrzysztofHerman <herman@ihp-microelectronics.com>

--- ihp-sg13g2/libs.tech/gnucap/models/build.sh
+++ ihp-sg13g2/libs.tech/gnucap/models/build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh 
 #
-gnucap-mg-vams -I ../../verilog-a/r3_cmc/ -I /usr/local/include/gnucap --cc  resistors_paramsets.vams | g++ -xc++ -I/usr/local/include/gnucap -fPIC -shared - -o ../plugins/resistors_paramsets.so
-gnucap-mg-vams -I /usr/local/include/gnucap --cc  resistors_stat.vams | g++ -xc++ -I/usr/local/include/gnucap -fPIC -shared - -o ../plugins/resistors_stat.so
+gnucap-mg-vams -I ../../verilog-a/r3_cmc/ -I /usr/local/include/gnucap -D res_typ  --cc  cornerRES.vams | g++ -xc++ -I/usr/local/include/gnucap -fPIC -shared - -o ../plugins/cornerRES_typ.so
+gnucap-mg-vams -I ../../verilog-a/r3_cmc/ -I /usr/local/include/gnucap -D res_bcs  --cc  cornerRES.vams | g++ -xc++ -I/usr/local/include/gnucap -fPIC -shared - -o ../plugins/cornerRES_bcs.so
+gnucap-mg-vams -I ../../verilog-a/r3_cmc/ -I /usr/local/include/gnucap -D res_wcs  --cc  cornerRES.vams | g++ -xc++ -I/usr/local/include/gnucap -fPIC -shared - -o ../plugins/cornerRES_wcs.so
+gnucap-mg-vams -I ../../verilog-a/r3_cmc/ -I /usr/local/include/gnucap -D res_stat --cc  cornerRES.vams | g++ -xc++ -I/usr/local/include/gnucap -fPIC -shared - -o ../plugins/cornerRES_stat.so
+
+#gnucap-mg-vams -I /usr/local/include/gnucap --cc  resistors_stat.vams | g++ -xc++ -I/usr/local/include/gnucap -fPIC -shared - -o ../plugins/resistors_stat.so
