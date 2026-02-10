cff6135f3a3fc333ff3741ef60fcedd2ad1ef3ee
KrzysztofHerman <herman@ihp-microelectronics.com>
2026-02-10 17:36:52 +0000

    Makefile changed to support the build process
    Signed-off-by: KrzysztofHerman <herman@ihp-microelectronics.com>

--- ihp-sg13g2/libs.tech/gnucap/models/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh 
-#
-#gnucap-mg-vams -I ../../verilog-a/r3_cmc/ -I /usr/local/include/gnucap -D res_typ  --cc  cornerRES.vams | g++ -xc++ -I/usr/local/include/gnucap -fPIC -shared -DNDEBUG - -o ../plugins/cornerRES_typ.so
-#gnucap-mg-vams -I ../../verilog-a/r3_cmc/ -I /usr/local/include/gnucap -D res_bcs  --cc  cornerRES.vams | g++ -xc++ -I/usr/local/include/gnucap -fPIC -shared -DNDEBUG - -o ../plugins/cornerRES_bcs.so
-#gnucap-mg-vams -I ../../verilog-a/r3_cmc/ -I /usr/local/include/gnucap -D res_wcs  --cc  cornerRES.vams | g++ -xc++ -I/usr/local/include/gnucap -fPIC -shared -DNDEBUG - -o ../plugins/cornerRES_wcs.so
-#gnucap-mg-vams -I ../../verilog-a/r3_cmc/ -I /usr/local/include/gnucap -D res_stat --cc  cornerRES.vams | g++ -xc++ -I/usr/local/include/gnucap -fPIC -shared -DNDEBUG - -o ../plugins/cornerRES_stat.so
-
-gnucap-mg-vams -I ../../verilog-a/r3_cmc/ -I /usr/local/include/gnucap -D cap_typ --cc  cornerCAP.vams | g++ -xc++ -I/usr/local/include/gnucap -fPIC -shared -DNDEBUG - -o ../plugins/cornerCAP_typ.so
